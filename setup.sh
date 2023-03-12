0e4fd8f3c942ad4f88d8b0b878b5b98fbc0dc6f3
Hilan Meyran <hilanmeyran@protonmail.com>
2023-03-12 17:34:32 +0000

    moved everythin to normal dotfiles repo on coder branch

--- setup.sh
+++ setup.sh
@@ -22,41 +22,4 @@ fi
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/bin
 
 # Install dotfiles
-chezmoi init --apply https://github.com/ShawnHeyli/dotfiles.git --branch proxmox
-
-### VSCODE ###
-
-extensions = (
-	catppuccin.catppuccin-vsc
-	github.copilot
-	eamodio.gitlens
-	pkief.material-icon-theme
-	pkief.material-product-icons
-	esbenp.prettier-vscode
-)
-
-for extension in "${extensions[@]}"
-do
-  code --install-extension "$extension"
-done
-
-declare -A vscode_settings=(
-    ["workbench.colorTheme"]="Catppuccin Macchiato"
-    ["editor.tabSize"]=2
-    ["files.autoSave"]="onFocusChange"
-    ["editor.formatOnSave"]= true,
-    ["editor.fontSize"]=16
-    ["editor.fontWeight"]= "500",
-    ["workbench.productIconTheme"]= "material-product-icons"
-    ["workbench.iconTheme"]= "material-icon-theme",
-    ["editor.fontFamily"]= "'ComicCodeLigatures Nerd Font Mono','BlexMono Nerd Font', 'IBM Plex Mono', 'Fira Code', 'Source Code Pro', Consolas",
-)
-
-for key in "${!vscode_settings[@]}"; do
-    value="${vscode_settings[$key]}"
-    if grep -q "\"$key\": *\".*\"" "$settings_file"; then
-        sed -i "s/\(\"$key\": *\".*\"\)/\"$key\": \"$value\"/g" "$settings_file"
-    else
-        echo "\"$key\": \"$value\"," >> "$settings_file"
-    fi
-done
+chezmoi init --apply https://github.com/ShawnHeyli/dotfiles.git --branch coder
